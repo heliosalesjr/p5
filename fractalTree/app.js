766a85cad6606c29883dfc41e555b1e22d2b8035
Helio Sales Jr <heliosalesjr@gmail.com>
2023-01-03 21:01:38 -0300

    fractal tree fix

--- fractalTree/app.js
+++ fractalTree/app.js
@@ -1,10 +1,11 @@
 function setup() {
   createCanvas(400, 400);
-  background(0);
   slider = createSlider(0, TWO_PI, PI /3, 0.01);
 }
 
 function draw() {
+  background(0);
+
   angle = slider.value();
   stroke(255);
   translate(200, height); // moves the beginning of the code here - before it was (0,0), now this is going to be the (0,0)
